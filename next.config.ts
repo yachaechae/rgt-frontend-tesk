dd4bb8cee5f3dfc1166512ffbeef495c50933c1a
yachaechae <wpql1207@gmail.com>
2024-12-09 22:58:37 +0900

    fix: 빌드시에도 env 읽을수있게 수정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+  reactStrictMode: true,
+  images: {
+    domains: ['books.google.com'],
+  },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+}
+export default nextConfig
